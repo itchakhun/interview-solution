b99bf767b0de0aef395a2f9eae606870d601130f
Itchakhun <16811182+itchakhun@users.noreply.github.com>
2021-08-01 10:49:30 +0700

    number validation

--- question-one/solution-1.js
+++ question-one/solution-1.js
@@ -2,17 +2,6 @@ const input = document.querySelector('input');
 const selectNumberType = document.getElementById('select-number-type');
 const result = document.getElementById('result');
 
-let value = '';
-input.addEventListener('input', ev => {
-  value = ev.target.value;
-  onInput()
-});
-
-let numberType = '0'; // 0 isPrime; 1 isFib
-selectNumberType.addEventListener('input', ev => {
-  numberType = ev.target.value
-  onInput()
-});
 
 function onInput() {
   const number = parseFloat(value);
@@ -27,14 +16,37 @@ function isPrime(number = 0) {
   return number > 1;
 }
 
-function isPerfectSquare(num) {
-  return num > 0 && Math.sqrt(num) % 1 === 0;
-}
+const isInteger = num => num % 1 === 0
+const isPerfectSquare = num => isInteger(Math.sqrt(num))
+
 
 function isFibonacci(val) {
   // (5*n2 + 4) or (5*n2 – 4) is a perfect square
   const initialVal = 5 * (val ** 2);
   return [initialVal + 4, initialVal - 4].some(isPerfectSquare);
 }
+
+let value = '';
+input.addEventListener('input', ev => {
+  const valueNumber = parseFloat(ev.target.value || 0);
+
+  if (!isInteger(valueNumber)) {
+    ev.target.value = Math.round(valueNumber).toString();
+  }
+
+  if (valueNumber < 0) {
+    ev.target.value = "1"
+  }
+
+  value = ev.target.value;
+
+  onInput()
+});
+
+let numberType = '0'; // 0 isPrime; 1 isFib
+selectNumberType.addEventListener('input', ev => {
+  numberType = ev.target.value
+  onInput()
+});
 
 onInput();
